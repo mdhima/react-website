8c297a48f3a26d75c4f63420cc9ab6b55beda1a9
mdhima <73672852+mdhima@users.noreply.github.com>
2021-08-28 00:10:51 -0400

    Add files via upload

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements';
+import { FaBars } from 'react-icons/fa';
+
+const Navbar = ({toggle}) => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/'>Magdalena</NavLogo>
+                    <MobileIcon onClick={toggle}>
+                        <FaBars/>
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to='about'>About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='movies'>Movies</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='skills'>Skills</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='find'>Find</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+                    </NavBtn>
+                </NavbarContainer>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
